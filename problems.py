107a64415d6509fea36e9073ac1fb049469b5f21
ImonuilSuleimanov <imonuil.suleimanov_2025@ucentralasia.org>
2022-06-30 15:13:05 +0600

    A new problem has been solved and added

--- problems.py
+++ problems.py
@@ -88,3 +88,14 @@ else:
 #        print(n)
 #else:
 #    print("Out of range!")
+
+
+#PROBLEM NUMBER SIX
+
+#a = int(input("enter a: "))
+#b = int(input("enter b: "))
+
+#if a*b <= 1000:
+ #   print(a*b)
+#else:
+#    print(a+b)
