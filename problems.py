103629df9e2683967ee0a9cabb28c364675fe434
ImonuilSuleimanov <imonuil.suleimanov_2025@ucentralasia.org>
2022-07-02 18:41:18 +0600

    Problem on reverse numbers solved and added

--- problems.py
+++ problems.py
@@ -229,3 +229,32 @@ else:
  #   for i in range(x):
   #      print(x, end=" ")
    # print("\n")
+   
+   
+   
+   
+#PROBLEM NUMBER 14
+
+#Write a program to check if the given number is a palindrome number.
+
+#A palindrome number is a number that is same after reverse. For example 545, is the palindrome numbers
+
+#def palindrome(number):
+    #print("original number", number)
+   # original_num = number
+    
+    # reverse the given number
+    #reverse_num = 0
+   # while number:
+       # reminder = number % 10
+      #  reverse_num = (reverse_num * 10) + reminder
+     #   number = number // 10
+
+    # check numbers
+    #if original_num == reverse_num:
+   #     print("Given number palindrome")
+  #  else:
+ #       print("Given number is not palindrome")
+
+#palindrome(111)
+#palindrome(125)
