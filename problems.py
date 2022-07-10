ac928ff9767857bf56e826486584d985971f5676
ImonuilSuleimanov <imonuil.suleimanov_2025@ucentralasia.org>
2022-07-10 14:26:48 +0600

    Few problems with while loop has been solved and added

--- problems.py
+++ problems.py
@@ -495,3 +495,33 @@ else:
 #while num <= 5:
  #   num = int(input("Enter a random number: "))
 #print("The last number you entered was", num)
+
+
+
+
+#PROBLEM NUMBER 32
+
+#    Ask the user to enter a
+#number and then enter
+#another number. Add these
+#two numbers together and
+#then ask if they want to add
+#another number. If they
+#enter “y", ask them to enter
+#another number and keep
+#adding numbers until they
+#do not answer “y”. Once the
+#loop has stopped, display
+#the total.
+
+#num1 = int(input("Enter a number: "))
+
+#total = num1
+
+#again = "y"
+
+#while again == "y":
+#        num2 = int(input("Enter another number: "))
+#        total = num1 + num2
+#        again = input("Do you want to add another number? ")
+#print("The total is ", total)
