03f5ffeeffa200a08ca825a6291822d37b3b4361
ImonuilSuleimanov <imonuil.suleimanov_2025@ucentralasia.org>
2022-07-31 02:07:36 +0600

    Few other problems about random were added

--- problems.py
+++ problems.py
@@ -707,3 +707,98 @@ else:
 #        print("You loose!") 
 #        
 #print("The number was ", num)
+
+#PROBLEM NUMBER 40
+
+#Make a maths quiz that asks five questions by randomly
+#generating two whole numbers to make the question
+#(e.g. [num1] + [num2]). Ask the user to enter the
+#answer. If they get it right add a point to their score. At
+#the end of the quiz, tell them how many they got correct
+#out of five.
+
+
+#import random
+
+#score = 0
+#count = 0
+
+#while count < 5:
+#    num1 = random.randint(1, 1000)
+#    print("Num1 is equal to: ", num1)
+#    num2 = random.randint(1,1000)
+#    print("Num2 is equal to: ", num2)
+#    guess = int(input("What is the sum of num1 and num2? "))
+#    if guess == num1+num2:
+#        score = score +1
+#    count = count+1
+#    print("Your score is: ", score)
+
+#Another solution
+
+#score = 0
+
+#for i in range(1,6):
+#    num1 = random.randint(1,50)
+#    num2 = random.randint(1,50)
+#    print("num1 is equal to", num1, "and num2 is", num2)
+#    guess = int(input("Enter the sum of num1 and num2: "))
+#    if guess == num1+num2:
+#        score = score + 1
+#print("Your overall score is: ", score)
+
+#PROBLEM NUMBER 41
+#Display five colours and ask the user to pick one. If they
+#pick the same as the program has chosen, say “Well
+#done”, otherwise display a witty answer which involves
+#the correct colour, e.g. “I bet you are GREEN with envy”
+#or “You are probably feeling BLUE right now”. Ask
+#them to guess again; if they have still not got it right,
+#keep giving them the same clue and ask the user to
+#enter a colour until they guess it correctly.
+
+#import random
+
+#colors = ["green", "blue", "red", "orange", "yellow"]
+#print("The given colors are: ",colors)
+#color = random.choice(colors)
+
+#guess = input("Guess the color computer picked: ")
+
+#while guess != color:
+#    if color == "green":
+#        guess = input("You might be feeling green today, try again: ")
+#    elif color == "blue":
+#        guess = input("You might be feeling blue today, try again: ")
+#    elif color == "red":
+#        guess = input("You might be feeling red today, try again: ")
+#    elif color == "orange":
+#        guess = input("You might be feeling orange today, try again: ")
+#    else:
+#        guess = input("You might be feeling yellow today, try again: ")
+#if guess == color:
+#    print("Well done!")
+#    quit()
+
+#Another solutions
+
+#color = random.choice(["green", "blue", "red", "orange", "yellow"])
+#theirchoice = True
+
+#while theirchoice == True:
+#    choice = input("Guess a color among green, blue, red, orange and yellow: ")
+#    choice = choice.lower()
+#    if choice == color:
+#        print("Well done!")
+#        theirchoice = False
+#    else:
+#        if color == "green":
+#            print("I am feeling green today, guess again: ")
+#        elif color == "blue":
+#            print("I am feeling blue today, guess again: ")
+#        elif color == "red":
+#            print("I am feeling red today, guess again: ")
+#        elif color == "orange":
+#            print("I am feeling orange today, guess again: ")
+#        elif color == "yellow":
+#            print("I am feeling yellow today, guess again: ")
--- test.py
+++
@@ -1,5 +0,0 @@
-#Randomly pick a whole number between 1
-#and 10. Ask the user to enter a number and
-#keep entering numbers until they enter the
-#number that was randomly picked.
-
