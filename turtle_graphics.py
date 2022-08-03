d536a5067f6eba0b62f509a8b19ef088b83e85b3
ImonuilSuleimanov <imonuil.suleimanov_2025@ucentralasia.org>
2022-08-03 21:20:36 +0600

    Problems on lists and touples

---
+++ turtle_graphics.py
@@ -0,0 +1,28 @@
+#Problem number 1
+
+#import turtle
+
+#turtle.shape("turtle")
+
+#for i in range(0,5):
+#    turtle.forward(100)
+#    turtle.right(72)
+    
+#turtle.exitonclick()
+
+#PROBLEM NUMBER 2
+
+#import turtle
+
+#turtle.shape("turtle")
+
+#for i in range(0,10):
+#    turtle.right(36)
+#    for i in range (0,5):
+#        turtle.forward(100)
+#        turtle.right(72)
+
+#turtle.exitonclick()
+
+
+
